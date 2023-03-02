2497a7a860d7a5814ef6b630a67858ea1d2b74fe
DESKTOP-1A84R9B\ThaiPhong <kim_thai_phong@allgrow-labo.jp>
2023-03-02 17:34:42 +0700

    first commit

---
+++ src/_public/assets/js/main.js
@@ -0,0 +1,12 @@
+$(".c-design__slickcontainer").slick({
+    dots: false,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    cssEase: "linear",
+    prevArrow: $(".c-design__prev"),
+    nextArrow: $(".c-design__next"),
+  });
